f3fb01f758b2535a019f0157e08d5a1f2aacf642
ritsudo <minato-namikaje@yandex.ru>
2024-07-12 23:15:52 +0300

    initial commit

---
+++ CLR_StudyProj_1/Program.cs
@@ -0,0 +1,8 @@
+﻿Object test = new string("test");
+string test2 = (string)test;
+
+Console.WriteLine(test);
+Console.WriteLine(test2);
+
+Console.WriteLine(test.GetType());
+Console.WriteLine(test2.GetType());
